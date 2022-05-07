d8d639e986a5853d68be8ee6898cdc11c1c776a5
3V4ND3R5ON <94180306+3V4ND3R5ON@users.noreply.github.com>
2022-05-06 21:31:31 -0300

    21:30 06/05

---
+++ Base/BLL/UsuarioBLL.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class UsuarioBLL
+    {
+        public void Inserir()
+        {
+
+        }
+        public void Excluir()
+        {
+
+        }
+        public void Alterar()
+        {
+
+        }
+        public List<Usuario> Buscar()
+        {
+            return new List<Usuario>();
+        }
+    }
+}
