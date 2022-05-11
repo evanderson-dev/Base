785b9774ccff51354edb0b40a2f0110b86b169b0
3V4ND3R5ON <94180306+3V4ND3R5ON@users.noreply.github.com>
2022-05-10 21:29:30 -0300

    21:30 10/05/22

---
+++ Base/DAL/UsuarioDAL.cs
@@ -0,0 +1,61 @@
+﻿using Model;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+// ALT + ENTER + ENTER = RETIRA AS BIBLIOTECAS DESNECESSARIAS
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public Usuario Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "";
+                SqlCommand cmd = new SqlCommand
+                {
+                    Connection = cn,
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = "SP_InserirUsuario"
+                };
+
+                SqlParameter pativo = new SqlParameter("@Ativo", SqlDbType.Bit)
+                {
+                    Value = _usuario.Ativo
+                };
+                cmd.Parameters.Add(pativo);
+                cmd.Parameters.Add(new SqlParameter("@NomeUsuario", SqlDbType.VarChar)
+                {
+                    Value = _usuario.NomeUsuario
+                });
+
+                SqlParameter psenha = new SqlParameter("@Senha", SqlDbType.VarChar)
+                {
+                    Value = _usuario.Senha
+                };
+                cmd.Parameters.Add(psenha);
+
+                SqlParameter pid = new SqlParameter("@Id", SqlDbType.Int)
+                {
+                    Value = _usuario.Id
+                };
+                cmd.Parameters.Add(pid);
+
+                cn.Open();
+                _usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                return _usuario;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("SERVIDOR SQL ERRO: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
