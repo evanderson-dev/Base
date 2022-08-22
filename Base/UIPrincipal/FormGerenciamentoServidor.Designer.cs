cc67bce49b7c6c6b3c0df61a2fbbfe986efb9772
Evanderson Ribeiro dos Santos <axel_steel@live.com>
2022-08-22 14:59:19 -0300

    REFATORAÇÃO DA TELA DE GERENCIAMENTO DE SERVIDORES

--- Base/UIPrincipal/FormGerenciamentoServidor.Designer.cs
+++ Base/UIPrincipal/FormGerenciamentoServidor.Designer.cs
@@ -35,6 +35,8 @@
             this.descricaoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.stringDeConexaoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.enderecoServidorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.buttonSair = new System.Windows.Forms.Button();
+            this.buttonAtualizar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGerenciamentoServidor)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.enderecoServidorBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -45,11 +47,12 @@
             this.buttonAdicionar.FlatAppearance.BorderSize = 0;
             this.buttonAdicionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAdicionar.Image = global::UIPrincipal.Properties.Resources.server_add;
-            this.buttonAdicionar.Location = new System.Drawing.Point(12, 10);
+            this.buttonAdicionar.Location = new System.Drawing.Point(31, 10);
             this.buttonAdicionar.Name = "buttonAdicionar";
             this.buttonAdicionar.Size = new System.Drawing.Size(23, 23);
             this.buttonAdicionar.TabIndex = 0;
             this.buttonAdicionar.UseVisualStyleBackColor = false;
+            this.buttonAdicionar.Click += new System.EventHandler(this.buttonAdicionar_Click);
             // 
             // buttonDeletar
             // 
@@ -57,7 +60,7 @@
             this.buttonDeletar.FlatAppearance.BorderSize = 0;
             this.buttonDeletar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDeletar.Image = global::UIPrincipal.Properties.Resources.server_delete;
-            this.buttonDeletar.Location = new System.Drawing.Point(41, 10);
+            this.buttonDeletar.Location = new System.Drawing.Point(60, 10);
             this.buttonDeletar.Name = "buttonDeletar";
             this.buttonDeletar.Size = new System.Drawing.Size(23, 23);
             this.buttonDeletar.TabIndex = 1;
@@ -78,8 +81,9 @@
             this.dataGridViewGerenciamentoServidor.Location = new System.Drawing.Point(2, 39);
             this.dataGridViewGerenciamentoServidor.Name = "dataGridViewGerenciamentoServidor";
             this.dataGridViewGerenciamentoServidor.RowHeadersVisible = false;
-            this.dataGridViewGerenciamentoServidor.Size = new System.Drawing.Size(829, 200);
+            this.dataGridViewGerenciamentoServidor.Size = new System.Drawing.Size(829, 307);
             this.dataGridViewGerenciamentoServidor.TabIndex = 4;
+            this.dataGridViewGerenciamentoServidor.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewGerenciamentoServidor_CellClick);
             // 
             // descricaoDataGridViewTextBoxColumn
             // 
@@ -98,16 +102,44 @@
             // 
             this.enderecoServidorBindingSource.DataSource = typeof(Model.EnderecoServidor);
             // 
+            // buttonSair
+            // 
+            this.buttonSair.BackColor = System.Drawing.Color.Transparent;
+            this.buttonSair.FlatAppearance.BorderSize = 0;
+            this.buttonSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSair.Image = global::UIPrincipal.Properties.Resources.cancel;
+            this.buttonSair.Location = new System.Drawing.Point(808, 10);
+            this.buttonSair.Name = "buttonSair";
+            this.buttonSair.Size = new System.Drawing.Size(23, 23);
+            this.buttonSair.TabIndex = 5;
+            this.buttonSair.UseVisualStyleBackColor = false;
+            this.buttonSair.Click += new System.EventHandler(this.buttonSair_Click);
+            // 
+            // buttonAtualizar
+            // 
+            this.buttonAtualizar.BackColor = System.Drawing.Color.Transparent;
+            this.buttonAtualizar.FlatAppearance.BorderSize = 0;
+            this.buttonAtualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAtualizar.Image = global::UIPrincipal.Properties.Resources.arrow_refresh;
+            this.buttonAtualizar.Location = new System.Drawing.Point(2, 10);
+            this.buttonAtualizar.Name = "buttonAtualizar";
+            this.buttonAtualizar.Size = new System.Drawing.Size(23, 23);
+            this.buttonAtualizar.TabIndex = 6;
+            this.buttonAtualizar.UseVisualStyleBackColor = false;
+            this.buttonAtualizar.Click += new System.EventHandler(this.buttonAtualizar_Click);
+            // 
             // FormGerenciamentoServidor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(834, 242);
+            this.ClientSize = new System.Drawing.Size(834, 349);
+            this.Controls.Add(this.buttonAtualizar);
+            this.Controls.Add(this.buttonSair);
             this.Controls.Add(this.dataGridViewGerenciamentoServidor);
             this.Controls.Add(this.buttonDeletar);
             this.Controls.Add(this.buttonAdicionar);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormGerenciamentoServidor";
@@ -130,5 +162,7 @@
         private System.Windows.Forms.BindingSource enderecoServidorBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn descricaoDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn stringDeConexaoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button buttonSair;
+        private System.Windows.Forms.Button buttonAtualizar;
     }
 }
--- Base/UIPrincipal/FormGerenciamentoServidor.cs
+++ Base/UIPrincipal/FormGerenciamentoServidor.cs
@@ -1,17 +1,23 @@
 ﻿using BLL;
+using Infra;
 using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace UIPrincipal
 {
     public partial class FormGerenciamentoServidor : Form
     {
+        string valorCelulaSelecionada;
         EnderecoServidorBLL enderecoServidorBLL = new EnderecoServidorBLL();
         public FormGerenciamentoServidor()
         {
             InitializeComponent();
         }
-
+        private void buttonSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
         private void FormGerenciamentoServidor_Load(object sender, EventArgs e)
         {
             try
@@ -23,19 +29,63 @@ namespace UIPrincipal
                 MessageBox.Show(ex.Message);
             }
         }
-
+        private void buttonAdicionar_Click(object sender, EventArgs e)
+        {
+            FormAddEnderecoServidor frm = new FormAddEnderecoServidor();
+            frm.ShowDialog();
+        }
+        private void dataGridViewGerenciamentoServidor_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                valorCelulaSelecionada = dataGridViewGerenciamentoServidor.Rows[e.RowIndex].Cells[0].Value.ToString();
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }        
         private void buttonDeletar_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Deseja realmente excluir este endereço?", "", MessageBoxButtons.YesNo) == DialogResult.No)
+            string caminhoCriptografado, caminhoDescriptografado;
+            string[] caminho = Directory.GetFiles(Constante.DiretorioDoEnderecoBanco, "BD*");
+            if (MessageBox.Show("CONFIRMA A EXCLUSÃO DESTE ENDEREÇO DE SERVIDOR?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    for (int i = 0; i < caminho.Length; i++)
+                    {
+                        caminhoCriptografado = File.ReadAllText(caminho[i]);
+                        caminhoDescriptografado = FuncoesGlobais.Base64Decode(caminhoCriptografado);
+                        if (caminhoDescriptografado.Contains(valorCelulaSelecionada))
+                        {
+                            File.Delete(caminho[i]);
+                            buttonAtualizar_Click(sender, e);
+                            MessageBox.Show($"O ARQUIVO {caminho[i]} FOI EXCLUÍDO!");
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
                 return;
-
-            int id;
-            //dataGridViewGerenciamentoServidor.CurrentRow.Selected = true;
-            //id = (int)dataGridViewGerenciamentoServidor.Rows[e.RowIndex].Cells[1].Value;
-            //id = Convert.ToInt32(((DataRowView)planoBindingSource.Current).Row["Id"]);
-
-            //enderecoServidorBLL.Excluir(id);
-            //planoBindingSource.RemoveCurrent();
+            }
+        }
+        private void buttonAtualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridViewGerenciamentoServidor.DataSource = enderecoServidorBLL.Buscar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
